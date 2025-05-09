42c4ad954e6222ea0a6e45c8d17a53112513fb1a
wpplng <lina.wappling@gmail.com>
2025-05-09 11:45:05 +0200

    user input parsed to int

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vänligen skriv in tre heltal, separerade med mellanslag.");
+            Console.WriteLine("Det första talet måste vara större än 1.");
+            Console.WriteLine("Det andra talet måste vara större än det första.");
+            Console.WriteLine("Det sista talet måste vara minst lika stort som det andra och inte högre än 100.");
+
+            string[] userValue = Console.ReadLine().Split(" ");
+            int x = Convert.ToInt32(userValue[0]);
+            int y = Convert.ToInt32(userValue[1]);
+            int n = Convert.ToInt32(userValue[2]);
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(n);
         }
     }
 }
